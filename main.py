01764a792f42a75d214c9af522b027c5b013b266
Adrian Robles <robles.adrian1994@gmail.com>
2026-05-19 19:01:36 -0300

    identando y ordando con document

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from utilidades import (
-    mostrar_menu,
-    pedir_opcion_menu
-)
+from utilidades import mostrar_menu, pedir_opcion_menu
 
-from validaciones import (
-    pedir_contrasena
-)
+from validaciones import pedir_contrasena
 
 from analisis import (
     validar_nivel_seguridad,
     buscar_caracter,
     mostrar_contrasena_invertida,
-    verificar_palindromo
+    verificar_palindromo,
 )
 
-from estadisticas import (
-    contar_tipos_caracteres,
-    generar_reporte_estadistico
-)
+from estadisticas import contar_tipos_caracteres, generar_reporte_estadistico
 
 
 def main() -> None:
@@ -31,11 +23,9 @@ def main() -> None:
     while True:
 
         mostrar_menu()
-
         opcion = pedir_opcion_menu()
 
         if opcion == 1:
-
             contrasena = pedir_contrasena()
 
             print("\nContraseña guardada correctamente.")
@@ -43,89 +33,55 @@ def main() -> None:
         elif opcion == 2:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
-
+                print("\nPrimero debe ingresar una contraseña.")
             else:
                 validar_nivel_seguridad(contrasena)
 
         elif opcion == 3:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
-
+                print("\nPrimero debe ingresar una contraseña.")
             else:
                 contar_tipos_caracteres(contrasena)
 
         elif opcion == 4:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
+                print("\nPrimero debe ingresar una contraseña.")
 
             else:
 
-                caracter = input(
-                    "\nIngrese un carácter a buscar: "
-                )
+                caracter = input("\nIngrese un carácter a buscar: ")
 
-                buscar_caracter(
-                    contrasena,
-                    caracter
-                )
+                buscar_caracter(contrasena, caracter)
 
         elif opcion == 5:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
+                print("\nPrimero debe ingresar " "una contraseña.")
 
             else:
-                mostrar_contrasena_invertida(
-                    contrasena
-                )
+                mostrar_contrasena_invertida(contrasena)
 
         elif opcion == 6:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
+                print("\nPrimero debe ingresar " "una contraseña.")
 
             else:
-                generar_reporte_estadistico(
-                    contrasena
-                )
+                generar_reporte_estadistico(contrasena)
 
         elif opcion == 7:
 
             if contrasena == "":
-                print(
-                    "\nPrimero debe ingresar "
-                    "una contraseña."
-                )
+                print("\nPrimero debe ingresar " "una contraseña.")
 
             else:
-                verificar_palindromo(
-                    contrasena
-                )
+                verificar_palindromo(contrasena)
 
         elif opcion == 8:
 
-            print(
-                "\nLa funcionalidad de ordenamiento "
-                "todavía no fue implementada."
-            )
+            print("\nLa funcionalidad de ordenamiento " "todavía no fue implementada.")
 
         elif opcion == 9:
 
